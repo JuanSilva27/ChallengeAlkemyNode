6ae1a4e41d62ab19e15a8bb9c90945c70d399841
JuanSilva27 <juan.silva.0270@gmail.com>
2022-01-24 18:29:47 -0300

    se crea ruta detail

--- src/controllers/charactersController.js
+++ src/controllers/charactersController.js
@@ -5,7 +5,7 @@ module.exports = {
     list: async (req, res) => {
         try {
             let personajes = await db.Personajes.findAll({
-                include: {all:true}
+                include: { all: true }
             })
             let data = personajes.map(personaje => {
                 let arr = {
@@ -29,8 +29,48 @@ module.exports = {
         }
     },
 
+    detail: async (req, res) => {
+        try {
+            let personaje = await db.Personajes.findByPk(req.params.id, {
+                include: { all: true }
+            })
+            let peliculas = personaje.peliculas.map(pelicula=>{
+                let arr={
+                    titulo:pelicula.titulo,
+                    imagen: pelicula.imagen,
+                    fecha:pelicula.fecha,
+                    calificacion:pelicula.calificacion
+                }
+                return arr
+            })
+            let data = {
+
+                imagen: personaje.imagen,
+                nombre: personaje.nombre,
+                edad: personaje.edad,
+                peso: personaje.peso,
+                historia: personaje.historia,
+                peliculas: peliculas
+
+
+            }
+            let response = {
+                status: 200,
+                message: "ok",
+                data
+            }
+            res.status(201).json(response)
+        }
+        catch (err) {
+            return res.status(404).json({
+                status: 404,
+                message: err + 1
+            })
+        }
+    },
+
     create: async (req, res) => {
-        let {imagen,nombre,edad,peso,historia}= req.body
+        let { imagen, nombre, edad, peso, historia } = req.body
         try {
             let data = await db.Personajes.create({
                 imagen: imagen,
@@ -49,16 +89,16 @@ module.exports = {
         catch (err) {
             return res.status(400).json({
                 status: 400,
-                message:"el Formato de creacion de Personaje debe ser image: string, nombre:string, edad: integer, peso: integer, historia: string"
+                message: "el Formato de creacion de Personaje debe ser image: string, nombre:string, edad: integer, peso: integer, historia: string"
             })
         }
 
     },
 
-    editById: async (req, res)=>{
-        try{
-            const Character= await db.Personajes.findByPk(req.params.id)
-            const UpdatedCharacter= await Character.update(req.body)
+    editById: async (req, res) => {
+        try {
+            const Character = await db.Personajes.findByPk(req.params.id)
+            const UpdatedCharacter = await Character.update(req.body)
             let response = {
                 status: 200,
                 message: "ok",
@@ -66,7 +106,7 @@ module.exports = {
             }
             res.status(201).json(response)
         }
-        catch(err){
+        catch (err) {
             return res.status(400).json({
                 status: 400,
                 message: err + "1"
@@ -75,10 +115,10 @@ module.exports = {
 
     },
 
-    deleteById: async (req,res)=>{
-        try{
+    deleteById: async (req, res) => {
+        try {
             await db.Personajes.destroy({
-                where:{
+                where: {
                     id: req.params.id
                 }
             })
@@ -88,7 +128,7 @@ module.exports = {
             }
             res.status(201).json(response)
         }
-        catch(err){
+        catch (err) {
             return res.status(404).json({
                 status: 404,
                 message: err + "1"
--- src/routes/characters.js
+++ src/routes/characters.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-let { list, create, editById, deleteById }=require("../controllers/charactersController")
+let { list, create, editById, deleteById, detail }=require("../controllers/charactersController")
 
 
 
 router.get("/",list)
+router.get("/detail/:id",detail)
 router.post("/create",create)
 router.put("/edit/:id",editById)
 router.delete("/delete/:id",deleteById)
