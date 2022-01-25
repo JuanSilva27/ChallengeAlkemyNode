ae1f26b990543237bb34de64f62e87946f301a05
JuanSilva27 <juan.silva.0270@gmail.com>
2022-01-24 22:09:13 -0300

    se termina el CRUD de peliculas

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -41,20 +41,20 @@ module.exports = {
             let pelicula = await db.PeliculasSeries.findByPk(req.params.id, {
                 include: [{ all: true }]
             })
-            let personajes= pelicula.personajes.map(element=>{
-                let arr={
+            let personajes = pelicula.personajes.map(element => {
+                let arr = {
                     id: element.id,
                     nombre: element.nombre
                 }
                 return arr
             })
 
-            let data= {
+            let data = {
                 id: pelicula.id,
                 imagen: pelicula.imagen,
                 titulo: pelicula.titulo,
                 fecha: pelicula.fecha,
-                calificacion:pelicula.calificacion,
+                calificacion: pelicula.calificacion,
                 genero: pelicula.genero.nombre,
                 personajes: personajes
             }
@@ -73,17 +73,17 @@ module.exports = {
         }
     },
 
-    create: async(req, res)=>{
-        
-        try{
+    create: async (req, res) => {
+
+        try {
             let { imagen, titulo, fecha, calificacion, id_genero } = req.body
-            if(id_genero<6 && id_genero>0){
-                let data= await db.PeliculasSeries.create({
-                    imagen:imagen,
-                    titulo:titulo,
-                    fecha:fecha,
-                    calificacion:calificacion,
-                    id_genero:id_genero
+            if (id_genero < 6 && id_genero > 0) {
+                let data = await db.PeliculasSeries.create({
+                    imagen: imagen,
+                    titulo: titulo,
+                    fecha: fecha,
+                    calificacion: calificacion,
+                    id_genero: id_genero
                 })
                 let response = {
                     status: 200,
@@ -91,14 +91,74 @@ module.exports = {
                     data
                 }
                 res.status(201).json(response)
-            }else{
+            } else {
                 res.status(400).json("el id_genero no corresponde a un genero existente en la DB")
             }
         }
-        catch (err){
+        catch (err) {
             res.status(400).json({
                 status: 400,
                 message: "el Formato de creacion de Personaje debe ser image: string, titulo:string, fecha: integer, calificacion: integer, id_genero: string (del 1 al 5)"
+            })
+        }
+    },
+
+    updateById: async (req, res) => {
+        try {
+            const pelicula = await db.PeliculasSeries.findByPk(req.params.id)
+            if (pelicula) {
+                const UpdatedMovie = await pelicula.update(req.body)
+                let response = {
+                    status: 200,
+                    message: "Pelicula editada",
+                    UpdatedMovie
+                }
+                res.status(201).json(response)
+            } else {
+                return res.status(404).json({
+                    status: 404,
+                    message: "pelicula no encontrada"
+                })
+            }
+        }
+        catch (err) {
+            return res.status(400).json({
+                status: 400,
+                message: err + "1"
+            })
+        }
+    },
+
+    deleteById: async (req, res) => {
+        try {
+            await db.PeliculasSeries.destroy({
+                where: {
+                    id: req.params.id
+                }
+            })
+
+            let cosas = await db.PersonajePelicula.findAll({ ///comprueba si la pelicula a eliminar esta relacionado a algun personaje
+                where: {
+                    id_pelicula: req.params.id
+                }
+            })
+            if (cosas.length > 0) { ///si esta relacionado a algun personaje elimina la relacion
+                db.PersonajePelicula.destroy({
+                    where: {
+                        id_pelicula: req.params.id
+                    }
+                })
+            }
+            let response = {
+                status: 200,
+                message: "La Pelicula fue eliminada"
+            }
+            res.status(201).json(response)
+        }
+        catch (err) {
+            return res.status(404).json({
+                status: 404,
+                message: "no se encontro el Personaje para eliminar"
             })
         }
     }
--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-let { list, detail, create }=require("../controllers/moviesController")
+let { list, detail, create, updateById, deleteById  }=require("../controllers/moviesController")
 
 
 
@@ -8,5 +8,7 @@ let { list, detail, create }=require("../controllers/moviesController")
 router.get("/",list)
 router.get("/detail/:id",detail)
 router.post("/create",create)
+router.put("/edit/:id", updateById)
+router.delete("/delete/:id", deleteById)
 
 module.exports=router
