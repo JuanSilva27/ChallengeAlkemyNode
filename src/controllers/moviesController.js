49cff856f7744d15252e97e2a1be392b217412fa
JuanSilva27 <juan.silva.0270@gmail.com>
2022-01-24 21:08:09 -0300

    se crea detalle de las peliculas

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -3,26 +3,72 @@ const db = require('../database/models')
 
 module.exports = {
     list: async (req, res) => {
-         
-            try {
-                let data = await db.PeliculasSeries.findAll({
-                    include:[
-                        {all: true}
-                    ]
-                })
-                let response={
-                    status:200,
-                    message:"ok",
-                    data
+
+        try {
+            let peliculas = await db.PeliculasSeries.findAll({
+                include: [
+                    { all: true }
+                ]
+            })
+
+            let data = peliculas.map(pelicula => {
+                let arr = {
+                    imagen: pelicula.imagen,
+                    titulo: pelicula.titulo,
+                    fecha: pelicula.fecha,
                 }
-                res.status(201).json(response)
+                return arr
+            })
+            let response = {
+                status: 200,
+                message: "ok",
+                data
             }
-            catch (error) {
-                return res.status(400).json({
-                    status : 400,
-                    message : error+"1"
-                })
+            res.status(201).json(response)
+        }
+        catch (error) {
+            return res.status(400).json({
+                status: 400,
+                message: error + "1"
+            })
+        }
+
+    },
+
+    detail: async (req, res) => {
+        try {
+            let pelicula = await db.PeliculasSeries.findByPk(req.params.id, {
+                include: [{ all: true }]
+            })
+            let personajes= pelicula.personajes.map(element=>{
+                let arr={
+                    id: element.id,
+                    nombre: element.nombre
+                }
+                return arr
+            })
+
+            let data= {
+                id: pelicula.id,
+                imagen: pelicula.imagen,
+                titulo: pelicula.titulo,
+                fecha: pelicula.fecha,
+                calificacion:pelicula.calificacion,
+                genero: pelicula.genero.nombre,
+                personajes: personajes
             }
-        
+            let response = {
+                status: 200,
+                message: "ok",
+                data
+            }
+            res.status(201).json(response)
+        }
+        catch (err) {
+            return res.status(400).json({
+                status: 400,
+                message: err + "1"
+            })
+        }
     }
 }
--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
-let { list }=require("../controllers/moviesController")
+let { list, detail }=require("../controllers/moviesController")
+
+
+
+
 router.get("/",list)
+router.get("/detail/:id",detail)
 
 module.exports=router
