364a883b5d51caf16efbb84ff032e49722ccb696
JuanSilva27 <juan.silva.0270@gmail.com>
2022-01-26 20:14:27 -0300

    se agrega tokens al login y register, y middlewares

--- src/routes/characters.js
+++ src/routes/characters.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 let { list, create, editById, deleteById, detail }=require("../controllers/charactersController")
-
+let authjwt=require("../middleware/authjwt")
 
 
 router.get("/",list)
 router.get("/detail/:id",detail)
-router.post("/create",create)
-router.put("/edit/:id",editById)
-router.delete("/delete/:id",deleteById)
+router.post("/create",authjwt,create)
+router.put("/edit/:id",authjwt,editById)
+router.delete("/delete/:id",authjwt,deleteById)
 
 
 module.exports=router
